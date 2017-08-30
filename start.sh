f90946bcdc69eeb935e07fa4098418c62a34f31f
Jenkins Continuous Integration Server <jenkins@bredt1-svwlgh01.cgmp-osa.com.br>
2017-08-30 15:17:57 -0300

    cleanup

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ else
 
    sh cronometro.sh &
    #Copiar fluxo de video da live para a pasta local
-   ffmpeg -i rtsp://192.168.42.1:554/live -crf 30 -preset ultrafast -r 30 -codec:v copy videoCamera.avi
+   ffmpeg -i rtsp://192.168.42.1:554/live -crf 30 -preset ultrafast -r 30 -codec:v copy videoCamera.avi &
  
 fi
 
