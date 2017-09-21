4ab88b50609bbcb85222309c93534cddd2ed81c7
sillas lima <sillas.lima@globo.com>
2017-09-21 09:57:07 -0300

    crono

---
+++ vide_extraido/crono1.py
@@ -0,0 +1,39 @@
+import datetime
+import time
+import threading
+
+def cronometro():
+	
+	try:
+		while opcao ==1 :
+			global i
+			time.sleep(1)
+			print('total segundos',i)
+			global x
+			x = datetime.timedelta(seconds=i)
+			i +=1
+			print(x)
+			#captura(x)
+		return (x)
+	except KeyboardInterrupt:
+	    print('concluido')
+
+def captura (tempo):
+	print(tempo)
+	captura = str(input('digite c para capturar '))
+	if captura == 'c':
+		corta = tempo
+		print(corta)
+	
+
+i=0
+x = datetime.timedelta(seconds=i)
+opcao = int(input('1-CAPTURAR 2- para Cancelar a verificação: '))
+if opcao ==1:
+	th = threading.Thread(target=cronometro, args=())
+	th.start()
+	#tempo=cronometro()
+	print(th)
+
+else:
+	print(a)
