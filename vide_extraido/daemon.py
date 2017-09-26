7c016a2c524b5caee8a3e3c8a9104539f9836ac9
sillas lima <sillas.lima@globo.com>
2017-09-26 18:48:29 -0300

    daemon python cronometro

---
+++ vide_extraido/daemon.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# daemon.py
+
+import os
+import sys
+import atexit
+import signal
+import datetime
+import time
+
+#from crono1 import cronometro
+
+def daemonize(pidfile, *, stdin='/dev/null',
+              stdout='/dev/null',
+              stderr='/dev/null'):
+    if os.path.exists(pidfile):
+        raise RuntimeError('Already running')
+
+    # First fork (detaches from parent)
+    try:
+        if os.fork() > 0:
+            raise SystemExit(0)  # Parent exit
+    except OSError as e:
+        raise RuntimeError('fork #1 failed.')
+
+    os.chdir('/')
+    os.umask(0)
+    os.setsid()
+    # Second fork (relinquish session leadership)
+    try:
+        if os.fork() > 0:
+            raise SystemExit(0)
+    except OSError as e:
+        raise RuntimeError('fork #2 failed.')
+
+    # Flush I/O buffers
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+    # Replace file descriptors for stdin, stdout, and stderr
+    with open(stdin, 'rb', 0) as f:
+        os.dup2(f.fileno(), sys.stdin.fileno())
+    with open(stdout, 'ab', 0) as f:
+        os.dup2(f.fileno(), sys.stdout.fileno())
+    with open(stderr, 'ab', 0) as f:
+        os.dup2(f.fileno(), sys.stderr.fileno())
+
+    # Write the PID file
+    with open(pidfile, 'w') as f:
+        print(os.getpid(), file=f)
+
+    # Arrange to have the PID file removed on exit/signal
+    atexit.register(lambda: os.remove(pidfile))
+
+    # Signal handler for termination (required)
+    def sigterm_handler(signo, frame):
+        raise SystemExit(1)
+
+    signal.signal(signal.SIGTERM, sigterm_handler)
+
+
+def main():
+    import time
+    import datetime
+
+    i = 0
+    #x = datetime.timedelta(seconds=i)
+    #sys.stdout.write('',x)
+    while True :
+        time.sleep(1)
+        i +=1
+
+        tempo_atual = datetime.timedelta(seconds=i)
+        print(str(tempo_atual),end='\n')
+
+if __name__ == '__main__':
+    PIDFILE = '/tmp/daemon.pid'
+
+    if len(sys.argv) != 2:
+        print('Usage: {} [start|stop]'.format(sys.argv[0]), file=sys.stderr)
+        raise SystemExit(1)
+
+    if sys.argv[1] == 'start':
+        try:
+            daemonize(PIDFILE,
+                      stdout='/tmp/tempo.txt',
+                      stderr='/tmp/dameon1.log')
+        except RuntimeError as e:
+            print(e, file=sys.stderr)
+            raise SystemExit(1)
+
+        main()
+
+    elif sys.argv[1] == 'stop':
+
+        if os.path.exists(PIDFILE):
+            with open(PIDFILE) as f:
+                os.kill(int(f.read()), signal.SIGTERM)
+        else:
+            print('Not running', file=sys.stderr)
+            raise SystemExit(1)
+
+
+    else:
+        print('Unknown command {!r}'.format(sys.argv[1]), file=sys.stderr)
+        raise SystemExit(1)
